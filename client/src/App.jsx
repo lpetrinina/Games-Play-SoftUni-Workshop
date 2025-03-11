9c6c8d7c94e29e944bec271f6d27dc1a44b9d802
Luiza Petrinina <petrinina95@gmail.com>
2025-03-11 16:51:55 +0200

    Extract header and home components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
 
 function App() {
-  return <div id="box"></div>;
+  return (
+    <div id="box">
+      <Header />
+
+      <main id="main-content">
+        <Home />
+      </main>
+    </div>
+  );
 }
 
 export default App;
