7fe7f606e2d4c521d0ae081fba7c7e3f7f06b235
Luiza Petrinina <petrinina95@gmail.com>
2025-03-11 17:28:11 +0200

    Add all main pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,10 @@ import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 
 import "./App.css";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
+import GameCreate from "./components/game-create/GameCreate";
+import GameCatalog from "./components/game-catalog/GameCatalog";
 
 function App() {
   return (
@@ -13,6 +17,10 @@ function App() {
       <main id="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/games" element={<GameCatalog />} />
+          <Route path="/games/create" element={<GameCreate />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </main>
     </div>
