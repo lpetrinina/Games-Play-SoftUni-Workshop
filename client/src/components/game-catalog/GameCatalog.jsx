f63e8be66408e4266c4fd88e259eab2536250302
Luiza Petrinina <petrinina95@gmail.com>
2025-03-12 13:25:37 +0200

    List games in catalog page

--- client/src/components/game-catalog/GameCatalog.jsx
+++ client/src/components/game-catalog/GameCatalog.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import gameService from "../../services/gameService";
+import GameCatalogItem from "./game-catalog-item/GameGatalogItem";
 
 export default function GameCatalog() {
   const [games, setGames] = useState([]);
@@ -15,37 +16,11 @@ export default function GameCatalog() {
   return (
     <section id="catalog-page">
       <h1>All Games</h1>
+
+      {games.map((game) => (
+        <GameCatalogItem key={game._id} {...game} />
+      ))}
       {/* <!-- Display div: with information about every game (if any) --> */}
-      <div className="allGames">
-        <div className="allGames-info">
-          <img src="/images/avatar-1.jpg" />
-          <h6>Action</h6>
-          <h2>Cover Fire</h2>
-          <a href="#" className="details-button">
-            Details
-          </a>
-        </div>
-      </div>
-      <div className="allGames">
-        <div className="allGames-info">
-          <img src="/images/avatar-1.jpg" />
-          <h6>Action</h6>
-          <h2>Zombie lang</h2>
-          <a href="#" className="details-button">
-            Details
-          </a>
-        </div>
-      </div>
-      <div className="allGames">
-        <div className="allGames-info">
-          <img src="/images/avatar-1.jpg" />
-          <h6>Action</h6>
-          <h2>MineCraft</h2>
-          <a href="#" className="details-button">
-            Details
-          </a>
-        </div>
-      </div>
 
       {/* <!-- Display paragraph: If there is no games  --> */}
       <h3 className="no-articles">No articles yet</h3>
